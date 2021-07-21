657877fd47d3596b7ab97d0c7986bb40b48678f5
Kim1am <ykimlam@163.com>
2021-07-21 17:24:30 +0800

    scroll组件

---
+++ src/components/scroll/index.js
@@ -0,0 +1,124 @@
+import React, { forwardRef, useEffect, useRef, useState, useImperativeHandle } from 'react'
+// 组件的 props 上进行类型检查，你只需配置特定的 propTypes 属性
+import PropTypes from 'prop-types'
+import BScroll from 'better-scroll'
+import styled from 'styled-components'
+const ScrollContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+const Scroll = forwardRef((props, ref) => {
+  const [bScroll, setBScroll] = useState()
+  // 个人理解为 定义一个ref ,其.current就是对应的DOM
+  const scrollContaninerRef = useRef()
+  // 组件属性
+  const { direction, click, refresh, pullUpLoading, pullDownLoading, bounceTop, bounceBottom } = props
+  const { pullUp, pullDown, onScroll } = props
+
+  useEffect(() => {
+    const scroll = new BScroll(scrollContaninerRef.current, {
+      scrollX: direction === 'horizental',
+      scrollY: direction === 'vertical',
+      probeType: 3,
+      click: click,
+      bounce: {
+        top: bounceTop,
+        bottom: bounceBottom,
+      },
+    })
+    setBScroll(scroll)
+    // TODO:如果你的 effect 返回一个函数，React 将会在执行清除操作时调用它：
+    return () => {
+      setBScroll(null)
+    }
+  }, [])
+
+  // 每次重新渲染都要刷新实例，防止无法滑动:
+  useEffect(() => {
+    if (refresh && bScroll) {
+      bScroll.refresh()
+    }
+  })
+
+  // 绑定scroll事件
+  useEffect(() => {
+    if (!bScroll || !onScroll) return
+    bScroll.on('scroll', scroll => {
+      onScroll(scroll)
+    })
+    return () => {
+      bScroll.off('scroll')
+    }
+  }, [onScroll, bScroll])
+
+  // 上拉到底的判断，调用上拉刷新的函数
+  useEffect(() => {
+    if (!bScroll || !pullUp) return
+    bScroll.on('scrollEnd', () => {
+      // 判断是否滑动到了底部
+      if (bScroll.y <= bScroll.maxScrollY + 100) {
+        pullUp()
+      }
+    })
+    return () => {
+      bScroll.off('scrollEnd')
+    }
+  }, [pullUp, bScroll])
+
+  // 下拉的判断，调用下拉刷新的函数
+  useEffect(() => {
+    if (!bScroll || !pullDown) return
+    bScroll.on('touchEnd', pos => {
+      // 判断用户的下拉动作
+      if (pos.y > 50) {
+        pullDown()
+      }
+    })
+    return () => {
+      bScroll.off('touchEnd')
+    }
+  }, [pullDown, bScroll])
+
+  //公开方法
+  useImperativeHandle(ref, () => ({
+    refresh() {
+      if (bScroll) {
+        bScroll.refresh()
+        bScroll.scrollTo(0, 0)
+      }
+    },
+    getBScroll() {
+      if (bScroll) {
+        return bScroll
+      }
+    },
+  }))
+  return <ScrollContainer ref={scrollContaninerRef}>{props.children}</ScrollContainer>
+})
+Scroll.defaultProps = {
+  direction: 'vertical',
+  click: true,
+  refresh: true,
+  onScroll: null,
+  pullUpLoading: false,
+  pullDownLoading: false,
+  pullUp: null,
+  pullDown: null,
+  bounceTop: true,
+  bounceBottom: true,
+}
+
+Scroll.propTypes = {
+  direction: PropTypes.oneOf(['vertical', 'horizental']),
+  refresh: PropTypes.bool,
+  onScroll: PropTypes.func,
+  pullUp: PropTypes.func,
+  pullDown: PropTypes.func,
+  pullUpLoading: PropTypes.bool,
+  pullDownLoading: PropTypes.bool,
+  bounceTop: PropTypes.bool, // 是否支持向上吸顶
+  bounceBottom: PropTypes.bool, // 是否支持向上吸顶
+}
+
+export default Scroll
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -7,6 +7,10 @@ function Slider(props) {
   const [sliderSwiper, setSliderSwiper] = useState(null)
   const { bannerList } = props
 
+  /*  副作用执行函数
+   * fn() 执行函数
+   * array[] 依赖项数组----“如果想执行只运行一次的 effect（仅在组件挂载和卸载时执行），可以传递一个空数组（[]）作为第二个参数。这就告诉 React 你的 effect 不依赖于 props 或 state 中的任何值，所以它永远都不需要重复执行。”
+   */
   useEffect(() => {
     if (bannerList.length && !sliderSwiper) {
       let newSliderSwiper = new Swiper('.slider-container', {
@@ -23,6 +27,7 @@ function Slider(props) {
 
   return (
     <SliderContainer>
+      <div className='before'></div>
       <div className='slider-container'>
         <div className='swiper-wrapper'>
           {bannerList.map(slider => {
