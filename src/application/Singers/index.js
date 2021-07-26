dd5fb42449b6418073c411542bdd98d353a75051
Kim1am <ykimlam@163.com>
2021-07-26 16:26:55 +0800

    singer silder part

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Horizen from '../../baseUI/horizen-item'
+import { categoryTypes, alphaTypes } from '../../api/mock'
+import { NavContainer } from './style'
 
-function Singers(props) {
-  return <div>Singers</div>
+function Singers() {
+  let [category, setCategory] = useState('')
+  let [alpha, setAlpha] = useState('')
+  let updateCategory = val => {
+    setCategory(val)
+  }
+  let updateAlpha = val => {
+    setAlpha(val)
+  }
+
+  return (
+    <NavContainer>
+      <Horizen list={categoryTypes} title={'分类 (默认热门):'} handleClick={updateCategory} oldVal={category}></Horizen>
+      <Horizen list={alphaTypes} title={'首字母:'} handleClick={updateAlpha} oldVal={alpha}></Horizen>
+    </NavContainer>
+  )
 }
 
 export default React.memo(Singers)
